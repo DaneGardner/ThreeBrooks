29467c6e85f3ea56554c33a23ead9bb435794204
Dane Gardner <dane.gardner@gmail.com>
2011-04-25 21:15:03 -0600

    Set license to LGPL 3.0, and fixed license header in each file

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,29 +1,29 @@
 /*!
-   \file 
+   \file MainWindow.cpp
    \author Dane Gardner <dane.gardner@gmail.com>
    \version 
 
    \section LICENSE
-   This file is part of the Open|SpeedShop Graphical User Interface
-   Copyright (C) 2010-2011 Argo Navis Technologies, LLC
+    This file is part of the ThreeBrooks homebrew recipe application
+    Copyright (C) 2011 Dane Gardner
 
-   This library is free software; you can redistribute it and/or modify it
-   under the terms of the GNU Lesser General Public License as published by the
-   Free Software Foundation; either version 2.1 of the License, or (at your
-   option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-   This library is distributed in the hope that it will be useful, but WITHOUT
-   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
-   for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-   You should have received a copy of the GNU Lesser General Public License
-   along with this library; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
    \section DESCRIPTION
 
  */
+
 
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
